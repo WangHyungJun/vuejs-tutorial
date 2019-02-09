b735d9d8bfa6a4599838393aec35b8d50794ed56
WangHyungJun <whj2013123218@gmail.com>
2019-02-09 17:48:44 +0900

    32-HttpRequest

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>
-    <form>
+    <form v-if="!submitted">
       <label>Blog Title:</label>
       <input type="text" v-model.lazy="blog.title" required/>
       <label>Blog Content:</label>
@@ -20,7 +20,11 @@
       <select v-model="blog.author">
         <option v-for="author in authors">{{author}}</option>
       </select>
+      <button v-on:click.prevent="post">Add Blog</button>
     </form>
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{blog.title}}</p>
@@ -46,7 +50,20 @@
               categories:[],
               author: ""
             },
-            authors:['김기홍', '최린범', '양예본', '왕형준']
+            authors:['김기홍', '최린범', '양예본', '왕형준'],
+            submitted: false
+          }
+      },
+      methods:{
+          post: function () {
+            this.$http.post("https://jsonplaceholder.typicode.com/posts",{
+              title: this.blog.title,
+              body: this.blog.content,
+              userId: 1
+            }).then(function (data) {
+              console.log(data);
+              this.submitted=true;
+            });
           }
       }
     }
